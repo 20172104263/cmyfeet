9aa576fb1c06c937fc07e05cf981a7a13a857685
20172104263 <39482017+20172104263@users.noreply.github.com>
2018-06-11 16:28:29 +0800

    Initial commit

---
+++ cmyfeet/cmyfeet.cpp
@@ -0,0 +1,69 @@
+// cmyfeet.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+class CFeet
+{
+protected:
+	int feet;
+	int inches;
+public:
+	CFeet();
+	void setfeet(int f);
+	void setinches(int ich);
+	void display();
+	int getfeet();
+	int getinches();
+
+};
+CFeet::CFeet()
+{
+	feet = 0;
+	inches = 0;
+
+}
+void CFeet::setfeet(int f)
+{
+	feet = f;
+}
+void CFeet::setinches(int ich)
+{
+	inches = ich % 12;
+	feet = feet + inches / 12;
+}
+void CFeet::display()
+{
+	cout << "基类" << feet << "英尺" << inches << "英寸" << endl;
+}
+int CFeet::getfeet()
+{
+	return feet;
+}
+int CFeet::getinches()
+{
+	return inches;
+}
+class CMyFeet :public CFeet
+{
+public:
+	void display();
+
+};
+void CMyFeet::display()
+{
+	cout << "派生类" << feet << "英尺" << inches << "英寸" << endl;
+}
+
+
+int main()
+{
+	CFeet *p;
+	p = new CMyFeet;
+	p->setfeet(3);
+	p->setinches(8);
+	p->display();
+	return 0;
+}
+
